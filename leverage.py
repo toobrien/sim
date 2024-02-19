502329f5be09d06bab12be23cbbcf15bea45b01c
Taylor O'Brien <toobrien@amazon.com>
2024-02-18 19:36:29 -0800

    .

--- leverage.py
+++ leverage.py
@@ -3,14 +3,18 @@ from    numpy                   import  cumsum
 from    numpy.random            import  normal
 import  plotly.graph_objects    as      go
 from    plotly.subplots         import  make_subplots
+from    sys                     import  argv
 
+
+PLOT            = int(argv[1])
+DOLLAR_STATS    = int(argv[2])
 
 if __name__ == "__main__":
     
     dpy             = 252
     es_price        = 5000 * 50
     rfr             = 0.0392
-    
+
     reward          = 200
     risk            = 500 / 2
     max_dd          = 2000
@@ -27,16 +31,26 @@ if __name__ == "__main__":
 
     sharpes         = [ 0.5, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0, strat_sharpe ]
     sigmas          = [ ((strat_mu - rfr) * sqrt(dpy)) / sharpe for sharpe in sharpes ]
+    names           = [ str(sharpe) for sharpe in sharpes ]
+    names[-1]       = "strategy"
 
-    print(f"{'':<10}{'mean':>10}{'stdev':>10}")
-    print(f"{spx_sharpe:<10.2f}{spx_mu / dpy:>10.4f}{spx_sigma * sqrt(1 / dpy):>10.4f}")
 
-    for i in range(len(sharpes)):
+    print(f"{'es_price:':<20}{es_price:<10.2f}")
+    print(f"{'reward:':<20}${reward:<10.2f}")
+    print(f"{'risk (p95):':<20}${risk * 2:<10.2f}")
+    print(f"{'max dd:':<20}${max_dd:<10.2f}")
+    print("\n")
+    
 
-        print(f"{sharpes[i]:<10.2f}{strat_mu:>10.4f}{sigmas[i]:>10.4f}")
+    if DOLLAR_STATS:
+    
+        print(f"{'name':<15}{'sharpe':<15}{'mean ($)':<15}{'stdev ($)':<15}{'failure (%)':<15}\n")
+        print(f"{'spx':<15}{spx_sharpe:<15.2f}{es_price * (e**(spx_mu / dpy) - 1):<15.2f}{es_price * (e**(spx_sigma * sqrt(1 / dpy)) - 1):<15.2f}{'-':<15}")
 
-    print()
-    print(f"{'sharpe':>15}{'failed (%)':>15}")
+    else:
+
+        print(f"{'name':<15}{'sharpe':<15}{'mean':<15}{'stdev':<15}{'failure (%)':<15}\n")
+        print(f"{'spx':<15}{spx_sharpe:<15.2f}{spx_mu / dpy:<15.4f}{spx_sigma * sqrt(1 / dpy):<15.4f}{'-':<15}")
 
     colors = {
         0.5:            "#0f112c",
@@ -51,7 +65,7 @@ if __name__ == "__main__":
     }
 
     n_charts        = len(colors) + 1
-    n_trials        = 10
+    n_trials        = 10000
     x               = [ i for i in range(dpy) ]
     fig             = make_subplots(
                         rows                = n_charts,
@@ -70,6 +84,7 @@ if __name__ == "__main__":
     for i in range(len(sharpes)):
 
         failed  = 0
+        name    = names[i]
         sharpe  = sharpes[i]
         sigma   = sigmas[i]
 
@@ -90,27 +105,40 @@ if __name__ == "__main__":
 
                     break
 
+            
+            if PLOT:
 
-            y = [ es_price * e**j - es_price for j in y ]
+                if DOLLAR_STATS:
 
-            trace = go.Scatter(
-                {
-                    "x":        x_,
-                    "y":        y,
-                    "marker":   { "color": color },
-                    "mode":     "markers",
-                    "name":     f"{i}<br>sharpe = {sharpe:0.2f}"
-                }
-            )
+                    y = [ es_price * e**j - es_price for j in y ]
 
-            fig.add_trace(trace, row = row, col = 1)
-            fig.add_trace(trace, row = n_charts, col = 1)
+                trace = go.Scatter(
+                    {
+                        "x":        x_,
+                        "y":        y,
+                        "marker":   { "color": color },
+                        "mode":     "markers",
+                        "name":     f"{i}<br>sharpe = {sharpe:0.2f}"
+                    }
+                )
 
-        row += 1
+                fig.add_trace(trace, row = row, col = 1)
+                fig.add_trace(trace, row = n_charts, col = 1)
+
+        row             += 1
+        failure_rate    =  failed / n_trials * 100
+
+        if DOLLAR_STATS:
+
+            print(f"{name:<15}{sharpe:<15.2f}{es_price * (e**(strat_mu) - 1):<15.2f}{es_price * (e**(sigma) - 1):<15.2f}{failure_rate:<15.1f}")
+
+        else:
+            
+            print(f"{name:<15}{sharpe:<15.2f}{strat_mu:<15.4f}{sigma:<15.4f}{failure_rate:<15.1f}")
         
-        print(f"{sharpe:>15.2f}{(failed / n_trials)*100:>15.2f}")
-        
-    fig.show()
+    if PLOT:
+
+        fig.show()
 
     pass
 
