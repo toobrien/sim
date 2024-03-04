e9188bce41f3b15cfcc1f4581e60a3b01651bc3b
Taylor O'Brien <toobrien@amazon.com>
2024-03-03 16:16:05 -0800

    .

---
+++ euro.py
@@ -0,0 +1,112 @@
+from    enum                    import  IntEnum
+from    math                    import  log
+from    numpy                   import  arange, array
+from    polars                  import  col
+import  plotly.express          as      px
+import  plotly.graph_objects    as      go
+from    plotly.subplots         import  make_subplots
+from    sklearn.linear_model    import  LinearRegression
+from    sys                     import  argv
+from    utils.rtools            import  get_spx_days
+
+
+class r(IntEnum):
+
+    datetime    = 0
+    open        = 1
+    high        = 2
+    low         = 3
+    close       = 4
+    date        = 5
+    time        = 6
+
+
+if __name__ == "__main__":
+
+    start   = argv[1]
+    end     = argv[2]
+
+    dates, dfs = get_spx_days(start, end)
+
+    to_ret      = {}
+    from_ret    = {}
+
+    for df in dfs:
+
+        df = df.filter((col("time") >= "09:30:00") & (col("time") <= "15:59:00"))
+
+        open_   = log(df.row(0)[r.open])
+        close_  = log(df.row(-1)[r.close])
+
+        for row in df.iter_rows():
+
+            time    = row[r.time]
+            close   = log(row[r.close])
+
+            if time not in to_ret:
+
+                to_ret[time] = []
+            
+            to_ret[time].append(close - open_)
+            
+            if time not in from_ret:
+                
+                from_ret[time] = []
+
+            from_ret[time].append(close_ - close)
+
+model   = LinearRegression()
+times   = sorted(list(to_ret.keys()))
+b_      = []
+r2_     = []
+
+for time in times:
+
+    x = array(to_ret[time]).reshape(-1, 1)
+    y = from_ret[time]
+
+    model.fit(x, y)
+
+    b = model.coef_[0]
+    a = model.intercept_
+
+    r2 = model.score(x, y)
+
+    if time == "12:30:00":
+
+        fig = px.scatter(x = to_ret[time], y = y, trendline = "ols")
+        fig.show()
+
+    #print(f"{time:<15}{b:<8.4f}{a:<8.4f}{r2:<8.4f}")
+
+    b_.append(b)
+    r2_.append(r2)
+
+fig = make_subplots(rows = 2, cols = 1)
+
+fig.add_trace(
+    go.Bar(
+        {
+            "x":    times,
+            "y":    b_,
+            "name": "beta"
+        }
+    ),
+    row = 1, col = 1
+)
+
+fig.add_trace(
+    go.Bar(
+        {
+            "x":    times[:-1],
+            "y":    r2_[:-1],
+            "name": "r^2"
+        }
+    ),
+    row = 2,
+    col = 1
+)
+
+fig.show()
+
+pass
--- utils/rtools.py
+++ utils/rtools.py
@@ -1,15 +1,13 @@
+from    bisect  import  bisect_left, bisect_right
 from    random  import  randint
-from    time    import  time
 import  polars  as      pl
 
 
 SPX_DF      = pl.DataFrame()
 SPX_DATES   = None
 
-def get_random_spx_days(
-        n:              int = 1,
-        max_days_back:  int = 0
-    ):
+
+def get_spx():
 
     global SPX_DF
     global SPX_DATES
@@ -21,10 +19,36 @@ def get_random_spx_days(
         SPX_DF      = SPX_DF.with_columns(pl.col("datetime").apply(lambda dt: dt.split()[1]).alias("time"))
         SPX_DATES   = sorted(list(set(list(SPX_DF["date"]))))
 
+    return SPX_DATES, SPX_DF
+
+
+def get_random_spx_days(
+        n:              int = 1,
+        max_days_back:  int = 0
+    ):
+
+    SPX_DATES, SPX_DF = get_spx()
+
     i           = -max_days_back if max_days_back else 0
     dates       = SPX_DATES[i:]
     selected    = [ dates[randint(0, len(dates) - 1)] for i in range(n) ]
     dfs         = [ SPX_DF.filter(pl.col("date") == date) for date in selected ]
     res         = [ dates, dfs ]
+
+    return res
+
+
+def get_spx_days(
+    start:  str = None, 
+    end:    str = None
+):
+
+    SPX_DATES, SPX_DF = get_spx()
+
+    i       = bisect_left(SPX_DATES, start) if start    else 0
+    j       = bisect_right(SPX_DATES, end)  if end      else None
+    dates   = SPX_DATES[i:j]
+    dfs     = [ SPX_DF.filter(pl.col("date") == date) for date in dates ]
+    res     = [ dates, dfs ]
 
     return res
