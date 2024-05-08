3547f751d4f9de8ff5751d00ce7f273847bdb15c
Taylor O'Brien <toobrien@amazon.com>
2024-05-08 15:21:24 -0700

    .

--- prop_2.py
+++ prop_2.py
@@ -1,3 +1,4 @@
+import  plotly.graph_objects    as      go
 from    math                    import  ceil, e, log, sqrt
 from    numpy                   import  cumsum, mean
 from    numpy.random            import  normal
@@ -5,8 +6,8 @@ from    sys                     import  argv
 from    typing                  import  List, Tuple
 
 
-#                  risk reward  leverage
-# python prop_2.py 1.0  1.0     1.0
+#                  risk reward  leverage    runs    trades_per_day  show_chart
+# python prop_2.py 1.0  1.0     1.0         1000    1               1
 
 
 DPY                         = 256
@@ -27,12 +28,10 @@ PROFIT_TARGET               = 3_000
 PROFIT_TARGET_PERCENT       = log((ES + PROFIT_TARGET) / ES)
 ACTIVATION_FEE              = 100
 PA_MONTHLY_FEE              = 135
-TRADES_PER_DAY              = 1
 COMMISSIONS_ALL_IN          = 4.0
 SPREAD                      = 12.5
 TRANSACTION_COSTS           = COMMISSIONS_ALL_IN + SPREAD
 TRANSACTION_COSTS_PERCENT   = log((ES + TRANSACTION_COSTS) / ES)
-RUNS                        = 10_000
 RUN_YEARS                   = 1
 
 #               size    eval ($/mo)     pa ($/mo)   activation fee  trailing dd     eval target
@@ -46,80 +45,96 @@ def sim_runs(
     days:               int,
     mu:                 float,
     sigma:              float,
-    leverage:           float
+    leverage:           float,
+    trades_per_day:     int,
+    show_chart:         bool
 ) -> List[Tuple]:
     
+    fig                 = go.Figure()
     failed              = 0
     passed              = 0
     returns             = []
     prop_fees           = []
     transaction_costs   = []
+    run_days            = []
 
     for i in range(runs):
 
         total_return            = 0
         total_prop_fees         = ACTIVATION_FEE
         total_transaction_costs = 0
-        trailing_drawdown       = DRAWDOWN_PERCENT
-        high_watermark          = 0
-        run                     = list(leverage * cumsum(normal(loc = mu, scale = sigma, size = days)))
+        run                     = leverage * cumsum(normal(loc = mu, scale = sigma, size = days)) - TRANSACTION_COSTS_PERCENT * trades_per_day
+        trailing_drawdown       = [ max(min(max(run[:i + 1]) + DRAWDOWN_PERCENT, 0), DRAWDOWN_PERCENT) for i in range(len(run)) ]
         blown                   = False
+        pt_hit                  = False
 
         for j in range(len(run)):
 
-            equity = run[j]
-
-            if equity > high_watermark:
-
-                high_watermark      = equity
-                trailing_drawdown   = min(high_watermark + DRAWDOWN_PERCENT, 0)
+            equity  = run[j]
+            trail   = trailing_drawdown[j]
             
-            elif equity <= trailing_drawdown:
+            if equity <= trail:
 
-                blown   =   True
-                failed  +=  1
-                run     =   run[0:j]
-
-                break
-
-        target_met_index = len(run)
-
-        for j in range(len(run)):
-
-            equity = run[j]
-
-            if equity >= PROFIT_TARGET_PERCENT:
-
-                passed              +=  1
-                target_met_index    =   j
+                blown               =   True
+                failed              +=  1
+                run                 =   run[0:j + 1]
+                trailing_drawdown   =   run[0:j + 1]
 
                 break
 
+            elif equity >= PROFIT_TARGET_PERCENT:
+
+                pt_hit = True
+
+        if pt_hit:
+
+            passed += 1
+
+        if show_chart:
+
+            fig.add_trace(
+                go.Scattergl(
+                    {
+                        "x":        [ i for i in range(len(run)) ],
+                        "y":        [ ES * e**(run[i]) for i in range(len(run)) ],
+                        "text":     [ f"{ES * e**trailing_drawdown[i]:0.2f}" for i in range(len(trailing_drawdown)) ],
+                        "marker":   { "color": "#FF0000" if blown else "#00FF00" if equity >= PROFIT_TARGET_PERCENT else "#0000FF" }
+                    }
+                )
+            )
         
         months                  = ceil(len(run) / DPM)
         total_prop_fees         = total_prop_fees + months * PA_MONTHLY_FEE
-        total_transaction_costs = (TRANSACTION_COSTS * TRADES_PER_DAY * len(run))
+        total_transaction_costs = (TRANSACTION_COSTS * trades_per_day * len(run))
         total_return            = equity if not blown and equity > PROFIT_TARGET_PERCENT else 0
         
-        returns.append(total_return)
+        run_days.append(len(run))
+        returns.append(total_return + log((ES + total_transaction_costs) / ES))
         prop_fees.append(total_prop_fees)
         transaction_costs.append(total_transaction_costs)
 
-    failure_rate                = failed / runs
-    passed_eval_rate            = passed / runs
-    average_return              = mean(returns)
-    average_prop_fees           = log(1 + mean(prop_fees) / ES)
-    average_transaction_costs   = log(1 + mean(transaction_costs) / ES)
-    excess_expected_return      = average_return - average_prop_fees - average_transaction_costs
+    failure_rate                    = failed / runs
+    passed_eval_rate                = passed / runs
+    average_return                  = mean(returns)
+    average_prop_fees               = log(1 + mean(prop_fees) / ES)
+    average_transaction_costs       = log(1 + mean(transaction_costs) / ES)
+    average_trading_days            = mean(run_days)
 
-    return failure_rate, passed_eval_rate, average_return, average_prop_fees, average_transaction_costs, excess_expected_return
+    return failure_rate, passed_eval_rate, average_return, average_prop_fees, average_transaction_costs, average_trading_days, fig
 
 
 if __name__ == "__main__":
 
+    reward          = float(argv[1])
+    risk            = float(argv[2])
+    leverage        = float(argv[3])
+    runs            = int(argv[4])
+    trades_per_day  = int(argv[5])
+    show_chart      = int(argv[6])
+
     print(f"t_bill:                     {T_BILL:0.4f}")
     print(f"t_bill_daily:               {T_BILL_DAILY:0.4f}")
-    print(f"es_price:                   {ES:0.2f}")
+    print(f"es_price:                   {ES / 50:0.2f}")
     print(f"es_mu:                      {ES_MU:0.4f}")
     print(f"es_mu_daily:                {ES_MU_DAILY:0.4f}")
     print(f"es_sigma:                   {ES_SIGMA:0.4f}")
@@ -129,13 +144,11 @@ if __name__ == "__main__":
     print(f"profit_target_percent:      {PROFIT_TARGET_PERCENT:0.4f}")
     print(f"drawdown:                   {DRAWDOWN}")
     print(f"drawdown_percent:           {DRAWDOWN_PERCENT:0.4f}")
-    print(f"transaction_costs_percent:  {TRANSACTION_COSTS_PERCENT:0.4f}\n")
-    print(f"runs:                       {RUNS}\n")
+    print(f"transaction_costs_percent:  {TRANSACTION_COSTS_PERCENT:0.4f}")
+    print(f"runs:                       {runs}")
+    print(f"trades_per_day:             {trades_per_day}\n")
     print("-----\n")
-    
-    reward      = float(argv[1])
-    risk        = float(argv[2])
-    leverage    = float(argv[3])
+
     mu          = reward * ES_MU_DAILY
     sigma       = risk * ES_SIGMA_DAILY
     sharpe      = (mu - T_BILL_DAILY) / sigma * sqrt(DPY)
@@ -145,20 +158,27 @@ if __name__ == "__main__":
         pass_rate, 
         average_return, 
         average_prop_fees, 
-        average_transaction_costs, 
-        excess_expected_return 
-    ) = sim_runs(RUNS, RUN_YEARS * DPY, mu, sigma, leverage)
+        average_transaction_costs,
+        average_trading_days,
+        fig
+    ) = sim_runs(runs, RUN_YEARS * DPY, mu, sigma, leverage, trades_per_day, show_chart)
 
     print(f"reward:                     {reward:0.2f}x")
     print(f"risk:                       {risk:0.2f}x")
     print(f"sharpe:                     {sharpe:0.2f}")
     print(f"survival rate:              {(1 - failure_rate) * 100:0.2f}%")
     print(f"eval pass rate:             {pass_rate * 100:0.2f}%")
+    print(f"average trading days:       {int(ceil(average_trading_days))}")
     print(f"average return:             {average_return * 100:0.2f}%\t${ES * e**average_return - ES:0.2f}")
     print(f"average prop fees:          {average_prop_fees * 100:0.2f}%\t${ES * e**average_prop_fees - ES:0.2f}")
     print(f"average transaction costs:  {average_transaction_costs * 100:0.2f}%\t${ES * e**average_transaction_costs - ES:0.2f}")
-    print(f"return after fees:          {excess_expected_return * 100:0.2f}%\t${ES * e**excess_expected_return - ES:0.2f}")
+
+    return_after_costs = (average_return - average_prop_fees - average_transaction_costs)
+
+    print(f"return after all costs:     {return_after_costs * 100:0.2f}%\t${ES * e**return_after_costs - ES:0.2f}")
 
     print("\n\n")
+
+    fig.show()
 
     pass
