05da4dbf2bbff7d1cee2279e7140da5d9a6d6875
Taylor O'Brien <toobrien@amazon.com>
2024-05-11 20:36:16 -0700

    .

--- prop_2.py
+++ prop_2.py
@@ -6,21 +6,24 @@ from    sys                     import  argv
 from    typing                  import  List, Tuple
 
 
-#                  reward risk   leverage runs trades_per_day show_chart
-# python prop_2.py 1.0x   1.0x   1.0      1000 1              1
-# python prop_2.py \$100  \$200  1.0      1000 1              1
-# python prop_2.py 2p     5p     1.0      1000 1              1
-# python prop_2.py 0.0003 0.0125 1.0      1000 1              1
+#                  reward risk   leverage runs trades_per_day withdrawal_frequency_days withdrawal_amount_dollars show_chart
+# python prop_2.py 1.0x   1.0x   1.0      1000 1              0                         0                         1
+# python prop_2.py \$100  \$200  1.0      1000 1              0                         0                         1
+# python prop_2.py 2p     5p     1.0      1000 1              21                        2000                      1
+# python prop_2.py 0.0003 0.0125 1.0      1000 1              63                        2000                      1
 
 # risk/reward can be ES multiplier, $, ES points, or basis points
 
 
-reward          = argv[1]
-risk            = argv[2]
-leverage        = float(argv[3])
-runs            = int(argv[4])
-trades_per_day  = int(argv[5])
-show_chart      = int(argv[6])
+reward                      = argv[1]
+risk                        = argv[2]
+leverage                    = float(argv[3])
+runs                        = int(argv[4])
+trades_per_day              = int(argv[5])
+withdrawal_frequency_days   = int(argv[6])
+withdrawal_amount_dollars   = float(argv[7])
+show_chart                  = int(argv[8])
+
 
 DPY                         = 256
 DPM                         = 21
@@ -45,6 +48,9 @@ COMMISSIONS_ALL_IN          = 4.0 if leverage >= 1.0 else 1.2
 SPREAD                      = 12.5 if leverage >= 1.0 else 1.25
 TRANSACTION_COSTS           = COMMISSIONS_ALL_IN + SPREAD
 TRANSACTION_COSTS_PERCENT   = log(1 + TRANSACTION_COSTS / ES)
+WITHDRAWAL_AMOUNT_PCT       = log(1 + withdrawal_amount_dollars / ES)
+PROFIT_SHARE_RATE           = 0.1
+PROFIT_SHARE_LIMIT          = 10_000
 RUN_YEARS                   = 1
 
 #               size    eval ($/mo)     pa ($/mo)   activation fee  trailing dd     eval target
@@ -54,22 +60,24 @@ RUN_YEARS                   = 1
 
 
 def sim_runs(
-    runs:               int,
-    days:               int,
-    mu:                 float,
-    sigma:              float,
-    leverage:           float,
-    trades_per_day:     int,
-    show_chart:         bool
+    runs:                       int,
+    days:                       int,
+    mu:                         float,
+    sigma:                      float,
+    leverage:                   float,
+    trades_per_day:             int,
+    withdrawal_frequency_days:  int,
+    show_chart:                 bool
 ) -> List[Tuple]:
     
     fig                 = go.Figure()
     failed              = 0
     passed              = 0
-    withdrawal_eligible = 0
+    total_withdraws     = 0
     returns             = []
     prop_fees           = []
     transaction_costs   = []
+    withdraws           = []
     run_days            = []
 
     for i in range(runs):
@@ -79,6 +87,7 @@ def sim_runs(
         total_transaction_costs = 0
         run                     = leverage * cumsum(normal(loc = mu, scale = sigma, size = days)) - TRANSACTION_COSTS_PERCENT * trades_per_day
         trailing_drawdown       = [ max(min(max(run[:i + 1]) + DRAWDOWN_PERCENT, 0), DRAWDOWN_PERCENT) for i in range(len(run)) ]
+        withdrawn               = 0
         blown                   = False
         pt_hit                  = False
 
@@ -100,13 +109,15 @@ def sim_runs(
 
                 pt_hit = True
 
+                if withdrawal_frequency_days and j % withdrawal_frequency_days == 0:
+
+                    total_withdraws += 1
+                    withdrawn       += WITHDRAWAL_AMOUNT_PCT
+                    run[j + 1:]     -= WITHDRAWAL_AMOUNT_PCT
+
         if pt_hit:
 
             passed += 1
-
-        if equity >= PROFIT_TARGET_PERCENT and j > MINIMUM_TRADING_DAYS:
-
-            withdrawal_eligible += 1
 
         if show_chart:
 
@@ -121,25 +132,33 @@ def sim_runs(
                 )
             )
         
+        if withdrawn > PROFIT_SHARE_LIMIT:
+
+            withdrawn = PROFIT_SHARE_LIMIT + (withdrawn - PROFIT_SHARE_LIMIT) * (1 - PROFIT_SHARE_RATE)
+
         months                  = ceil(len(run) / DPM)
         total_prop_fees         = total_prop_fees + months * PA_MONTHLY_FEE
         total_transaction_costs = (TRANSACTION_COSTS * trades_per_day * len(run))
         total_return            = equity if not blown and equity > PROFIT_TARGET_PERCENT else 0
-        
+        total_return            = total_return + log(1 + total_transaction_costs / ES)
+        total_return            = total_return + log(1 + withdrawn / ES)
+
         run_days.append(len(run))
-        returns.append(total_return + log(1 + total_transaction_costs / ES))
+        returns.append(total_return)
         prop_fees.append(total_prop_fees)
         transaction_costs.append(total_transaction_costs)
+        withdraws.append(withdrawn)
 
     failure_rate                    = failed / runs
     passed_eval_rate                = passed / runs
-    withdrawal_rate                 = withdrawal_eligible / runs
+    withdrawal_rate                 = total_withdraws / runs
     average_return                  = mean(returns)
     average_prop_fees               = log(1 + mean(prop_fees) / ES)
     average_transaction_costs       = log(1 + mean(transaction_costs) / ES)
     average_trading_days            = mean(run_days)
+    average_withdrawn               = mean(withdraws)
 
-    return failure_rate, passed_eval_rate, withdrawal_rate, average_return, average_prop_fees, average_transaction_costs, average_trading_days, fig
+    return failure_rate, passed_eval_rate, withdrawal_rate, average_return, average_prop_fees, average_transaction_costs, average_trading_days, average_withdrawn, fig
 
 
 def get_metric(x, es_x):
@@ -186,7 +205,7 @@ if __name__ == "__main__":
     print(f"drawdown_percent:               {DRAWDOWN_PERCENT:0.4f}")
     print(f"commissions (rt):               {COMMISSIONS_ALL_IN:0.2f}")
     print(f"spread:                         ${SPREAD:0.2f}")
-    print(f"transaction_costs_percent:      {TRANSACTION_COSTS_PERCENT:0.5f}")
+    print(f"transaction_costs_bp:           {TRANSACTION_COSTS_PERCENT:0.5f}")
     print("\n-----\n")
 
     mu_bp, mu_dollars       = get_metric(reward, ES_MU_DAILY)
@@ -194,13 +213,13 @@ if __name__ == "__main__":
     sharpe                  = (mu_bp - T_BILL_DAILY) / sigma_bp * sqrt(DPY)
     sharpe_0                = mu_bp / sigma_bp * sqrt(DPY)
 
-    print(f"reward:                         {reward}\t{mu_bp * 100:0.2f}%\tw/ leverage: ${mu_dollars * leverage:0.2f}\t{mu_bp * 100 * leverage:0.4f}%")
-    print(f"risk:                           {risk}\t{sigma_bp * 100:0.2f}%\tw/ leverage: ${sigma_dollars * leverage:0.2f}\t{sigma_bp * 100 * leverage:0.4f}%")
+    print(f"daily reward:                   {reward}\t{mu_bp * 100:0.2f}%\tw/ leverage: ${mu_dollars * leverage:0.2f}\t{mu_bp * 100 * leverage:0.4f}%")
+    print(f"daily risk:                     {risk}\t{sigma_bp * 100:0.2f}%\tw/ leverage: ${sigma_dollars * leverage:0.2f}\t{sigma_bp * 100 * leverage:0.4f}%")
     print(f"leverage:                       {leverage:0.2f}x")
     print(f"runs:                           {runs}")
     print(f"trades_per_day:                 {trades_per_day}\n")
     print(f"sharpe (rfr = {T_BILL * 100:0.2f}%):           {sharpe:0.2f}")
-    print(f"sharpe (rfr = 0):               {sharpe_0:0.2f}")
+    print(f"sharpe (rfr = 0):               {sharpe_0:0.2f}\t({sharpe_0 / ES_SHARPE_0:0.2f}x better risk-adjusted return than ES)")
 
     print("\n-----\n")
 
@@ -209,19 +228,30 @@ if __name__ == "__main__":
         pass_rate,
         withdrawal_rate,
         average_return, 
-        average_prop_fees, 
+        average_prop_fees,
         average_transaction_costs,
         average_trading_days,
+        average_withdrawn,
         fig
-    ) = sim_runs(runs, RUN_YEARS * DPY, mu_bp, sigma_bp, leverage, trades_per_day, show_chart)
+    ) = sim_runs(
+        runs, 
+        RUN_YEARS * DPY, 
+        mu_bp, 
+        sigma_bp, 
+        leverage, 
+        trades_per_day, 
+        withdrawal_frequency_days, 
+        show_chart
+    )
 
     print(f"survival rate:                  {(1 - failure_rate) * 100:0.2f}%")
     print(f"eval pass rate:                 {pass_rate * 100:0.2f}%")
-    print(f"withdrawal available:           {withdrawal_rate * 100:0.2f}%")
+    print(f"withdraws per account:          {withdrawal_rate:0.2f}")
     print(f"average trading days:           {int(ceil(average_trading_days))}")
     print(f"average return:                 {average_return * 100:0.2f}%\t${ES * (e**average_return - 1):0.2f}")
     print(f"average prop fees:              {average_prop_fees * 100:0.2f}%\t${ES * (e**average_prop_fees - 1):0.2f}")
     print(f"average transaction costs:      {average_transaction_costs * 100:0.2f}%\t${ES * (e**average_transaction_costs - 1):0.2f}")
+    print(f"average_withdrawn:              ${average_withdrawn:0.2f}")
 
     return_after_costs = (average_return - average_prop_fees - average_transaction_costs)
 
