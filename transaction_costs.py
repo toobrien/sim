26f2cea944878fdb221895402e1ac161d27f5c6c
Taylor O'Brien <toobrien@amazon.com>
2024-07-27 22:54:02 -0700

    .

--- transaction_costs.py
+++ transaction_costs.py
@@ -1,7 +1,9 @@
 import  plotly.graph_objects    as      go
 from    numpy                   import  array, cumsum, max, min, mean
 from    random                  import  choices
+from    sys                     import  argv
 from    time                    import  time
+
 
 
 if __name__ == "__main__":
@@ -9,12 +11,15 @@ if __name__ == "__main__":
     t0          = time()
     n           = 10_000
     trades      = 1000
-    tp          = 200
-    sl          = -200
+    tp          = float(argv[1])
+    sl          = float(argv[2])
+    equity      = 2500
+    p_win       = abs(sl) / (tp + sl)
+    p_lose      = 1 - p_win
     costs       = 15
     X           = [ i for i in range(1, trades + 1) ]
-    no_costs    = array([ cumsum(choices([tp, sl], [ 0.5, 0.5 ], k = trades )) for _ in range(n) ])
-    with_costs  = array([ cumsum(choices([tp - costs, sl - costs], [ 0.5, 0.5 ], k = trades)) for _ in range(n) ])
+    no_costs    = array([ cumsum(choices([tp, -sl], [ p_win, p_lose ], k = trades )) for _ in range(n) ])
+    with_costs  = array([ cumsum(choices([tp - costs, -sl - costs], [ p_win, p_lose ], k = trades)) for _ in range(n) ])
 
     no_costs_m      = mean(no_costs, axis = 0)
     no_costs_h      = max(no_costs, axis = 0)
@@ -31,6 +36,8 @@ if __name__ == "__main__":
                         ( with_costs_l, f"min pnl, with costs", "#FF0000")
                     ]
     
+    # pnl
+
     fig = go.Figure()
 
     for trace in traces:
@@ -38,14 +45,62 @@ if __name__ == "__main__":
         fig.add_trace(
             go.Scatter(
                 {
-                    "x": X,
-                    "y": trace[0],
-                    "name": trace[1],
-                    "marker": { "color": trace[2] }
+                    "x":        X,
+                    "y":        trace[0],
+                    "name":     trace[1],
+                    "marker":   { "color": trace[2] }
                 }
             )
         )
         
+    fig.show()
+
+    # survival rate
+
+    no_costs_survival   = array([ 0. for i in range(n) ])
+    with_costs_survival = array([ 0. for i in range(n) ])
+
+    for arrs in [ 
+        ( no_costs, no_costs_survival ),
+        ( with_costs, with_costs_survival )
+    ]:
+
+        in_arr  = arrs[0]
+        out_arr = arrs[1]
+
+        for i in range(n):
+
+            for j in range(trades):
+
+                if in_arr[i][j] <= -equity:
+
+                    break
+
+                else:
+
+                    out_arr[j] += 1
+
+    no_costs_survival /= n
+    with_costs_survival /= n
+
+    fig     = go.Figure()
+    traces  = [
+                ( no_costs_survival, "survival rate, without costs", "#0000FF" ),
+                ( with_costs_survival, "survival rate, with costs", "#FF0000" )
+            ]
+
+    for trace in traces:
+
+        fig.add_trace(
+            go.Scatter(
+                {
+                    "x":        X,
+                    "y":        trace[0],
+                    "name":     trace[1],
+                    "marker":   { "color": trace[2] }
+                }
+            )
+        )
 
     fig.show()
 
